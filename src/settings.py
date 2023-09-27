941912386e16a3c8b3d7c0b62997217f3f35c547
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-09-27 14:47:38 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Cows2021"
 PROJECT_NAME_FULL: str = "Cows2021 Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
